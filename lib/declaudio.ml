5a1451eb7fa451b115ba2b34aee933b7347f1a7e
Yiming Xiang <yiming@cs.utexas.edu>
2025-12-23 09:21:01 +0800

    chore: format

--- lib/declaudio.ml
+++ lib/declaudio.ml
@@ -1,45 +1,42 @@
 open Js_of_ocaml
 
-
-let stop_sound (node_id : int32) = 
-    Js.Unsafe.obj
+let stop_sound (node_id : int32) =
+  Js.Unsafe.obj
     [|
       ("action", Js.Unsafe.inject (Js.string "stopSound"));
       ("nodeGroupId", Js.Unsafe.inject (Js.int32 node_id));
     |]
 
 let set_volume (node_id : int32) (volume : float) =
-    Js.Unsafe.obj
+  Js.Unsafe.obj
     [|
       ("action", Js.Unsafe.inject (Js.string "setVolume"));
       ("nodeGroupId", Js.Unsafe.inject (Js.int32 node_id));
       ("volume", Js.Unsafe.inject (Js.float volume));
     |]
+
 let set_volume_at (node_id : int32) (volumeat : float) =
-    Js.Unsafe.obj
+  Js.Unsafe.obj
     [|
       ("action", Js.Unsafe.inject (Js.string "setVolumeAt"));
       ("nodeGroupId", Js.Unsafe.inject (Js.int32 node_id));
       ("volumeAt", Js.Unsafe.inject (Js.float volumeat));
     |]
 
-type loop_config = {
-  loop_start: float;
-  loop_end: float;
-}
+type loop_config = { loop_start : float; loop_end : float }
 
-let get_loop_start (cfg: loop_config option) : Js.Unsafe.any =
+let get_loop_start (cfg : loop_config option) : Js.Unsafe.any =
   match cfg with
   | Some c -> Js.Unsafe.inject @@ Js.float c.loop_start
-  | None -> Js.Unsafe.inject Js.null 
+  | None -> Js.Unsafe.inject Js.null
 
-let get_loop_end (cfg: loop_config option) : Js.Unsafe.any =
+let get_loop_end (cfg : loop_config option) : Js.Unsafe.any =
   match cfg with
   | Some c -> Js.Unsafe.inject @@ Js.float c.loop_end
   | None -> Js.Unsafe.inject Js.null
 
-let set_loop_config (node_id : int32) (loop: loop_config option) =
-    Js.Unsafe.obj
+let set_loop_config (node_id : int32) (loop : loop_config option) =
+  Js.Unsafe.obj
     [|
       ("action", Js.Unsafe.inject (Js.string "setLoopConfig"));
       ("nodeGroupId", Js.Unsafe.inject (Js.int32 node_id));
@@ -48,31 +45,37 @@ let set_loop_config (node_id : int32) (loop: loop_config option) =
     |]
 
 let set_playback_rate (node_id : int32) (playback_rate : float) =
-    Js.Unsafe.obj
+  Js.Unsafe.obj
     [|
       ("action", Js.Unsafe.inject (Js.string "setPlaybackRate"));
       ("nodeGroupId", Js.Unsafe.inject (Js.int32 node_id));
       ("playbackRate", Js.Unsafe.inject (Js.float playback_rate));
     |]
 
-type volume_timelines = ((float * float) list) list 
+type volume_timelines = (float * float) list list
 
-let gen_volume_timelines: volume_timelines -> Js.Unsafe.any =
-  fun vts ->
-    let timeline_to_js (timeline: (float * float) list) =
-      let points = Array.map (fun (t, v) ->
-        Js.Unsafe.obj [|
-          ("time", Js.Unsafe.inject (Js.float t));
-          ("volume", Js.Unsafe.inject (Js.float v));
-        |]
-      ) (Array.of_list timeline) in
-      (Js.array points)
+let gen_volume_timelines : volume_timelines -> Js.Unsafe.any =
+ fun vts ->
+  let timeline_to_js (timeline : (float * float) list) =
+    let points =
+      Array.map
+        (fun (t, v) ->
+          Js.Unsafe.obj
+            [|
+              ("time", Js.Unsafe.inject (Js.float t));
+              ("volume", Js.Unsafe.inject (Js.float v));
+            |])
+        (Array.of_list timeline)
     in
-    let timelines_js = List.map timeline_to_js vts in
-    Js.Unsafe.inject (Js.array (Array.of_list timelines_js))
+    Js.array points
+  in
+  let timelines_js = List.map timeline_to_js vts in
+  Js.Unsafe.inject (Js.array (Array.of_list timelines_js))
 
-let start_sound (node_id : int32) (buffer_id : int32) (volume : float) (volume_timelines : volume_timelines) (start_time : float) (start_at : float) (loop : loop_config option) (playback_rate : float) =
-    Js.Unsafe.obj
+let start_sound (node_id : int32) (buffer_id : int32) (volume : float)
+    (volume_timelines : volume_timelines) (start_time : float)
+    (start_at : float) (loop : loop_config option) (playback_rate : float) =
+  Js.Unsafe.obj
     [|
       ("action", Js.Unsafe.inject (Js.string "startSound"));
       ("nodeGroupId", Js.Unsafe.inject (Js.int32 node_id));
@@ -87,7 +90,7 @@ let start_sound (node_id : int32) (buffer_id : int32) (volume : float) (volume_t
     |]
 
 let load_audio (audio_url : string) (req_id : int32) =
-    Js.Unsafe.obj
+  Js.Unsafe.obj
     [|
       ("audioUrl", Js.Unsafe.inject (Js.string audio_url));
       ("requestId", Js.Unsafe.inject (Js.int32 req_id));
--- test/test_ml_declaudio.ml
+++ test/test_ml_declaudio.ml
@@ -3,4 +3,14 @@ open Ml_declaudio
 
 let cmd1 = Declaudio.load_audio "sd" 2l
 
+let timelines1 : Declaudio.volume_timelines =
+  [ [ (0., 0.0); (1., 1.0); (2., 0.5) ]; [ (3., 0.0); (4., 0.8); (10., 0.0) ] ]
 
+let cmd2 = Declaudio.start_sound 1l 2l 3. timelines1 2. 2. None 3.
+
+let export_app () =
+  Js.export "App"
+    (Js.Unsafe.obj
+       [| ("cmd1", Js.Unsafe.inject cmd1); ("cmd2", Js.Unsafe.inject cmd2) |])
+
+let _ = export_app ()
